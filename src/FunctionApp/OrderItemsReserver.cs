0df877150bda4ea8b10667831a642ff5706c3b53
ferzik85 <sersid@bk.ru>
2022-09-22 11:32:11 +0200

    save

---
+++ src/FunctionApp/OrderItemsReserver.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Newtonsoft.Json;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using System;
+
+namespace FunctionApp
+{
+    public static class OrderItemsReserver
+    {
+        [FunctionName("OrderItemsReserver")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Order items request received");
+
+            string storageConfigConnectionString = System.Environment.GetEnvironmentVariable("BlobConnectionString");
+            string storageConfigFileContainerName = System.Environment.GetEnvironmentVariable("BlobFileContainerName");
+
+            var order = await new StreamReader(req.Body).ReadToEndAsync();        
+            var orderId = JsonConvert.DeserializeObject<Order>(order).Id;
+
+            var blobServiceClient = new BlobServiceClient(storageConfigConnectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(storageConfigFileContainerName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient($"Order {orderId}");
+            await blobClient.UploadAsync(BinaryData.FromString(order), overwrite: true);
+
+            var responseMessage = "Order items request successfully uploaded to blob storage";
+            log.LogInformation(responseMessage);
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
--- src/Web/Pages/Basket/Checkout.cshtml.cs
+++ src/Web/Pages/Basket/Checkout.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Configuration;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,6 +9,7 @@ using Microsoft.eShopWeb.ApplicationCore.Exceptions;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.Infrastructure.Identity;
 using Microsoft.eShopWeb.Web.Interfaces;
+using Newtonsoft.Json;
 
 namespace Microsoft.eShopWeb.Web.Pages.Basket;
 
@@ -19,18 +22,20 @@ public class CheckoutModel : PageModel
     private string _username = null;
     private readonly IBasketViewModelService _basketViewModelService;
     private readonly IAppLogger<CheckoutModel> _logger;
+    private IConfiguration _configuration;
 
     public CheckoutModel(IBasketService basketService,
         IBasketViewModelService basketViewModelService,
         SignInManager<ApplicationUser> signInManager,
         IOrderService orderService,
-        IAppLogger<CheckoutModel> logger)
+        IAppLogger<CheckoutModel> logger, IConfiguration configuration)
     {
         _basketService = basketService;
         _signInManager = signInManager;
         _orderService = orderService;
         _basketViewModelService = basketViewModelService;
         _logger = logger;
+        _configuration = configuration;
     }
 
     public BasketViewModel BasketModel { get; set; } = new BasketViewModel();
@@ -53,7 +58,10 @@ public class CheckoutModel : PageModel
 
             var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
             await _basketService.SetQuantities(BasketModel.Id, updateModel);
-            await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"));
+            var orderId = await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"));
+            var order = await _orderService.GetOrderAsync(orderId);
+            var funcUri = _configuration["FuncUri"];
+            await InitOrderRequestAsync(funcUri, order);
             await _basketService.DeleteBasketAsync(BasketModel.Id);
         }
         catch (EmptyBasketOnCheckoutException emptyBasketOnCheckoutException)
@@ -64,6 +72,13 @@ public class CheckoutModel : PageModel
         }
 
         return RedirectToPage("Success");
+    }
+
+    private async Task InitOrderRequestAsync(string uri, string payload)
+    {
+        using var client = new HttpClient();
+        var stringContent = new StringContent(payload, Encoding.UTF8, "application/json");
+        var stringTask = await client.PostAsync(uri, stringContent);
     }
 
     private async Task SetBasketModelAsync()
